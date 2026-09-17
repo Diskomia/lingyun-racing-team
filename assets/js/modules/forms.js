d470d3352cc97c5b02cfe60a75b0b572cfd27234
Diskomia <diskomiakhan@gmail.com>
2026-09-17 11:14:25 +0800

    fix: back to GAS endpoint

--- assets/js/modules/forms.js
+++ assets/js/modules/forms.js
@@ -20,7 +20,7 @@
   const FormDispatcher = {
     targetEmail: 'diskomiakhan@gmail.com',
     endpoint: 'https://formsubmit.co/ajax/diskomiakhan@gmail.com',
-    gasEndpoint: 'https://formsubmit.co/ajax/diskomiakhan@gmail.com',
+    gasEndpoint: 'https://script.google.com/macros/s/AKfycbxokikkqxMlEYXQr2RAbHU218VagoeyxxmaAAW-ngcioI3PdcO7b7zugckuTcROxvWdLg/exec', // 凌云油车队 GAS 邮件中枢
 
     init() {
       this.initContactForm();
